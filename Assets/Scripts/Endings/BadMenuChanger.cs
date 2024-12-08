4fe3ae5185330f9b340d15ae82eb6bc84b597e11
VK Vusuwandla <vamseevkv@gmail.com>
2024-12-08 01:59:22 -0800

    Updating Bad End credits sequence

--- Assets/Scripts/Endings/BadMenuChanger.cs
+++ Assets/Scripts/Endings/BadMenuChanger.cs
@@ -7,11 +7,13 @@ public class BadMenuChanger : MonoBehaviour
 {
     [SerializeField] private string sceneName;
     [SerializeField] private Animator endScreenAnimator;
+    [SerializeField] private PlayerController playerController;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
+            if (playerController != null) playerController.enabled = false;
             if (endScreenAnimator != null)
             {
                 endScreenAnimator.SetTrigger("PlayEnding"); // Trigger End Credits animation
@@ -22,7 +24,7 @@ public class BadMenuChanger : MonoBehaviour
 
     IEnumerator ExitToEndMenu()
     {
-        yield return new WaitForSeconds(20f); // Wait for the animation to finish
+        yield return new WaitForSeconds(16f); // Wait for the animation to finish
         SceneManager.LoadScene(sceneName); // Load the EndMenu scene
     }
 }
